a7b8e44126199f5be7834d974e0b083a125e24d6
shadownater <shadownater@hotmail.com>
2017-02-22 18:14:07 -0700

    Post and comment added to application

--- socialdistribution/urls.py
+++ socialdistribution/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'socialdistribution.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^posts/', include('posts.urls', namespace='posts')),
     url(r'^admin/', include(admin.site.urls)),
 ]
