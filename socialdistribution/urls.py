d3bc7b4f156041024fb091eba2c3f2675255ca4e
nlovas <nlovas@ualberta.ca>
2017-02-23 16:47:20 -0700

    you can register new users but cant log in yet

--- socialdistribution/urls.py
+++ socialdistribution/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^posts/', include('posts.urls', namespace='posts')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^authors/register/$', 'authors.views.register', name='register'),
+    url(r'^authors/register/complete/$', 'authors.views.registration_complete',
+ name='registration_complete'),
 ]
