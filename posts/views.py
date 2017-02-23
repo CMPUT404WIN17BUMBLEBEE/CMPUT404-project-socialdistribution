ada7e633eaa8ff9d89d1eb03cf3e6a32536f8dc7
shadownater <shadownater@hotmail.com>
2017-02-22 19:21:54 -0700

    Displays posts in a link form, no comments yet and only can post from admin atm

--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render
-
+from datetime import datetime, timedelta
 from django.http import HttpResponse
+from django.template import Context, loader
 from .models import Post, Comment
 
 # Create your views here.
 
-def index(request):
+#parts of code from http://pythoncentral.io/writing-simple-views-for-your-first-python-django-application/
 
-	latest_posts_list = Post.objects.order_by('-pub_date')[:5]
+def index(request):
+	two_days_ago = datetime.utcnow() - timedelta(days=2)
+
+	latest_posts_list = Post.objects.filter(date_created__gt=two_days_ago).all()
+
+	#template = loader.get_template('index.html')
 
 	context = {
 	
-	'lastest_posts_list': latest_posts_list
+	'latest_posts_list': latest_posts_list
 
 	}
 
