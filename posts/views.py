839223b45db84a3752521d9459e6d94cab3b5b1f
shadownater <shadownater@hotmail.com>
2017-02-22 19:03:07 -0700

    Getting something to show up with template

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import Post, Comment
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("This is the posts application")
+
+	latest_posts_list = Post.objects.order_by('-pub_date')[:5]
+
+	context = {
+	
+	'lastest_posts_list': latest_posts_list
+
+	}
+
+	return render(request, 'posts/index.html', context)
