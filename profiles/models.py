d1c0c9d302b2519300bdf480a35535b2f65dc36f
mep6 <mep6@ualberta.ca>
2017-02-26 17:56:17 -0700

    New profile module, with basic model filled in. No views or templates yet.

---
+++ profiles/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+@python_2_unicode_compatible
+class Profile(models.Model):
+    userId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    displayName = models.CharField(max_length=200)
+    githubUsername = models.CharField(max_length=200)
+    firstName = models.CharField(max_length=200)
+    lastName = models.CharField(max_length=200)
+    email = models.CharField(max_length=400)
+    bio = models.CharField(max_length=2000)
