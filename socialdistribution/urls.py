e9dd5c2a96b1c8d85d986c3a9b5d32fbbb0aefca
mep6 <mep6@ualberta.ca>
2017-03-05 14:14:01 -0700

    Fixed an issue with routing to add a post.

--- socialdistribution/urls.py
+++ socialdistribution/urls.py
@@ -12,21 +12,19 @@ urlpatterns = [
     url(r'^posts/', include('posts.urls', namespace='posts')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^posts/(?P<post_id>\d+)/detail.html$', 'thebuzz.views.post_detail', name='post_detail'),	
-    url(r'^posts/form_upload.html$', 'thebuzz.views.post_form_upload', name='post_form_upload'),
+    url(r'^posts/(?P<post_id>\d+)/detail.html$', 'thebuzz.views.post_detail', name='post_detail'),
+    url(r'^posts/post_form_upload.html$', 'thebuzz.views.post_form_upload', name='post_form_upload'),
     url(r'^posts/(?P<post_id>\d+)/add_comment.html$', 'thebuzz.views.add_comment', name='add_comment'),
 
- 
+
     url(r'^register/$', 'thebuzz.views.register', name='register'),
     url(r'^register/complete/$', 'thebuzz.views.registration_complete', name='registration_complete'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^home/$', 'thebuzz.views.homePage', name='homePage'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/login/'}, name='logout'),
 
-    
-    
+
+
     url(r'^api/', include(router.urls))
 
 ]
-
-
