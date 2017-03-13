e464a4dac43c95fe7eccd10d79daeaa178c2cd30
semorris <semorris@ualberta.ca>
2017-03-13 09:58:31 -0600

    change to settings.py for heroku

--- socialdistribution/settings.py
+++ socialdistribution/settings.py
@@ -113,7 +113,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_DIR,'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR,'staticfiles')
 
 #help with this from
 #catherine on Stack OverFlow
