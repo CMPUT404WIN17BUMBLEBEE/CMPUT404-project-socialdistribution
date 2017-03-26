abfbc928adb1392134063d7effb1dcb9abe3856e
semorris <semorris@ualberta.ca>
2017-03-26 14:41:48 -0600

    undo change

--- thebuzz/views.py
+++ thebuzz/views.py
@@ -273,9 +273,7 @@ def get_Post(post_id):
         try:
             api_user = Site_API_User.objects.get(site_id = site.id)
             resp = requests.get(api_url, auth=(api_user.username, api_user.password))
-            #post = resp.json()
-            data = json.loads(resp.text)
-            post = data["posts"]
+            post = resp.json()
         #Results in an AttributeError if the object does not exist at that site
         except AttributeError:
             #Setting isPostData to False since that site didn't have the data
