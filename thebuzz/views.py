a9979600836da8274f838e5c3667e2418f7110cf
nlovas <nlovas@ualberta.ca>
2017-04-01 17:07:56 -0600

    just got github friends working. that was pretty tough

--- thebuzz/views.py
+++ thebuzz/views.py
@@ -274,8 +274,8 @@ def posts(request):
     return render(request, 'posts/posts.html', context)
 
 
-@login_required(login_url = '/login/')
-def createGithubPosts(request):
+#@login_required(login_url = '/login/')
+def DELETEMEcreateGithubPosts(request):
 #get github activity of myself - and create posts to store in the database
 
     #if not request.user.is_authenticated:
@@ -351,7 +351,7 @@ def createGithubPosts(request):
 			    post.save()
 			    postlist.append(post)
 		
-			createFriendsGithubs( user ) #returns postlist of ppl youre following
+			createFriendsGithubs( request ) #returns postlist of ppl youre following
 
 			#if there is nothing new to send, send an empty array
 			if(len(postlist) is 0):	
@@ -375,35 +375,126 @@ def createGithubPosts(request):
 			    jtmp[index]['currentId'] = str(user.id) #current logged in user's id #
 			    index += 1
 
-			print(json.dumps(jtmp))
+			#print(json.dumps(jtmp))
 			return HttpResponse(json.dumps(jtmp),content_type = "application/json")
 
-def createFriendsGithubs(user):
+#def createFriendsGithubs(request):
+@login_required(login_url = '/login/')
+def createGithubPosts(request):
 #generates the github posts of your friends. (visibility for github posts are FRIENDS so only get friend github posts!)
 #creates and returns a list of posts of ones that haven't been posted yet
 
-    print len(user.get_all_friends())
+    user = request.user.profile
     friends = user.get_all_friends() #array of usernames of my friends
     #next, get their githubs, if they have them, otherwise don't bother keeping them
     fgithubs = []
     mostRecent = [] #keeps track of most recent post by each friend
     index = 0
     while(index<len(friends)):
-	tmp =  Profile.objects.get(user__username=friends[index]).github
+	tmp =  Profile.objects.get(id=friends[index].id).github
 	if(tmp is not ""):
-	    mostRecent.append(Post.objects.filter(title = "Github Activity", associated_author = user).order_by('-published').first())
+	    mostRecent.append(Post.objects.filter(title = "Github Activity", associated_author =Profile.objects.get(id = friends[index].id)).order_by('-published').first())
+	    #print tmp
             fgithubs.append(tmp)
 	index += 1
 
-    #jdata = [][]
-    #index = 0
-    #while(index<len(fgithubs)):
-     #   resp = requests.get("https://api.github.com/users/" + fgithubs[index] + "/events") #gets newest to oldest events
-	#jdata[] = resp.json()
-	#if('https://developer.github.com/v3/#rate-limiting' in jdata[index]): #limit has been exceeded, wait 1 hour
+    if(user.github is not ""): #your own github posts are retrieved too!
+        mostRecent.append(Post.objects.filter(title = "Github Activity", associated_author =user.id).order_by('-published').first())
+	fgithubs.append(user.github)
 
-	#index += 1
+    jdata = []
+    index = 0
+    while(index<len(fgithubs)):
+        resp = requests.get("https://api.github.com/users/" + fgithubs[index] + "/events") #gets newest to oldest events
+	
+	jdata.append(resp.json())
+	if('https://developer.github.com/v3/#rate-limiting' in jdata[index]): #limit has been exceeded, wait 1 hour
+	    return HttpResponse(status=204)
 
+	index += 1
+
+
+    avatars = []
+    gtitle = "Github Activity"
+    contents = []
+    pubtime = []
+    postlist = []
+    index2 = 0
+    
+
+    #get the data
+    while(index2<len(jdata)):
+	    for item in jdata[index2]:
+		if(mostRecent[index2] is not None):
+		    cmpareDate = dateutil.parser.parse(item['created_at'])
+
+		    if(cmpareDate<=mostRecent[index2].published): #is the latest github post newer than the retrieved ones?dont create duplicates
+			continue
+
+		avatars.append(item['actor']['avatar_url']) 
+		pubtime.append(item['created_at'])
+
+		if( "commits" in item['payload'] ):
+		    #if there is commit data
+		    if( not item['payload']['commits']): #empty commit
+			    contents.append(item['type'] + " by " + item['actor']['display_login'] + " in <a href = 'https://github.com/" + item['repo']['name'] + "'> " + item['repo']['name'] + "</a> <br/>")
+		    else:
+		        contents.append(item['type'] + " by " + item['actor']['display_login'] +" (" + item['payload']['commits'][0]['author']['email'] + ")" + " in <a href = 'https://github.com/" + item['repo']['name'] + "'> " + item['repo']['name'] + "</a> <br/> \"" + item['payload']['commits'][0]['message'] + "\"")
+					 
+		else:
+		   #there is no commit data
+		    contents.append(item['type'] + " by " + item['actor']['display_login'] + " in <a href = 'https://github.com/" + item['repo']['name'] + "'> " + item['repo']['name'] + "</a> <br/>")
+
+	    index2 +=1	
+    #make posts for the database
+    print len(contents)
+    for i in range(0,len(contents)):
+	lilavatar = "<img src='" + avatars[i] + "'/>"
+
+	post = Post.objects.create(title = gtitle,
+	          content= lilavatar + "<p>" + contents[i] ,
+	          published=pubtime[i],
+	          associated_author = user,
+		  source = 'http://127.0.0.1:8000/',
+		  origin = 'huh',
+		  description = contents[i][0:97] + '...',
+		  visibility = 'FRIENDS',
+		  visibleTo = '',
+				               )
+	myImg = Img.objects.create(associated_post = post,
+	 				       myImg = lilavatar )
+	post.origin = 'http://' + request.get_host() + '/api' + reverse('post_detail', kwargs={'post_id': str(post.id) })
+	post.source = 'http://' + request.get_host() + '/api' + reverse('post_detail', kwargs={'post_id': str(post.id) })
+	#post.save()
+	postlist.append(post)
+	print len(postlist)
+	    
+
+#if there is nothing new to send, send an empty array
+    if(len(postlist) is 0):	
+	return HttpResponse(status=204)	
+	#prepare the new posts to be sent to the Ajax
+    jtmp = []
+			#print(len(postlist))
+			#print(postlist[0].id)
+    #print len(pubtime)		
+    index = 0
+    while(index<len(postlist)):
+			   
+       jtmp.append(model_to_dict(postlist[index]))
+       #print(jtmp[index])
+       jtmp[index]['image'] = ""#base64.b64encode(jtmp[index]['image']) TODO fix me
+       jtmp[index]['associated_author'] = str(Profile.objects.get(id = jtmp[index]['associated_author']).id)
+       jtmp[index]['id'] = str(postlist[index].id)
+       jtmp[index]['published'] = json.dumps(dateutil.parser.parse(pubtime[index] ).strftime('%B %d, %Y, %I:%M %p'))
+       jtmp[index]['published'] = jtmp[index]['published'][1:-1]
+       jtmp[index]['displayName'] = user.displayName
+       jtmp[index]['currentId'] = str(user.id) #current logged in user's id #
+       index += 1
+
+    #print(json.dumps(jtmp))
+    return HttpResponse(json.dumps(jtmp),content_type = "application/json")
+		
 
 def get_Post(post_id):
     post = {}
