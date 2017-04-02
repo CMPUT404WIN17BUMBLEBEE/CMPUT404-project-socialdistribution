6974584776365bdae965511fd1c2ad9c347b6787
nlovas <nlovas@ualberta.ca>
2017-04-01 18:37:24 -0600

    posts look good and origin/source problem fixed

--- thebuzz/views.py
+++ thebuzz/views.py
@@ -273,111 +273,6 @@ def posts(request):
 
     return render(request, 'posts/posts.html', context)
 
-
-#@login_required(login_url = '/login/')
-def DELETEMEcreateGithubPosts(request):
-#get github activity of myself - and create posts to store in the database
-
-    #if not request.user.is_authenticated:
-	#return HttpResponse(status=204)
-    #if request.user.is_authenticated:
-	    if request.method == 'GET':
-		    user = request.user.profile
-		    
-		    if not user.github.strip():
-			return HttpResponse(status=204)
-		    #make a GET request to github for my github name, if I have one
-		    if(user.github):
-
-			#first get the most recent github post, so we can stop if we hit this time or later
-			postQuery = Post.objects.filter(title = "Github Activity", associated_author = user).order_by('-published').first()
-
-			rurl = 'https://api.github.com/users/' + user.github + '/events'
-			resp = requests.get(rurl) #gets newest to oldest events
-			jdata = resp.json()
-		
-			avatars = []
-			gtitle = "Github Activity"
-			contents = []
-			pubtime = []
-			count = 0
-			postlist = [] #for sending a response
-
-			if('https://developer.github.com/v3/#rate-limiting' in jdata): #limit has been exceeded, wait 1 hour
-			    
-			    return HttpResponse(status=204)
-		
-			#get the data
-			for item in jdata:
-			    if(postQuery is not None):
-				    cmpareDate = dateutil.parser.parse(item['created_at'])
-
-				    if(cmpareDate<=postQuery.published): #is the latest github post newer than the retrieved ones?dont create duplicates
-					continue
-
-			    avatars.append(item['actor']['avatar_url']) #TODO:implement this after images with text is fixed
-			    pubtime.append(item['created_at'])
-
-			    if( "commits" in item['payload'] ):
-			    #if there is commit data
-				    if( not item['payload']['commits']): #empty commit
-					    contents.append(item['type'] + " by " + item['actor']['display_login'] + " in <a href = 'https://github.com/" + item['repo']['name'] + "'> " + item['repo']['name'] + "</a> <br/>")
-				    else:
-					    contents.append(item['type'] + " by " + item['actor']['display_login'] +" (" + item['payload']['commits'][0]['author']['email'] + ")" + " in <a href = 'https://github.com/" + item['repo']['name'] + "'> " + item['repo']['name'] + "</a> <br/> \"" + item['payload']['commits'][0]['message'] + "\"")
-				 #           count += 1
-			    else:
-			    #there is no commit data
-				    contents.append(item['type'] + " by " + item['actor']['display_login'] + " in <a href = 'https://github.com/" + item['repo']['name'] + "'> " + item['repo']['name'] + "</a> <br/>")
-			#            count += 1
-
-			#make posts for the database
-			for i in range(0,len(contents)):
-			    lilavatar = "<img src='" + avatars[i] + "'/>"
-
-			    post = Post.objects.create(title = gtitle,
-				                       content= lilavatar + "<p>" + contents[i] ,
-				                       published=pubtime[i],
-						       associated_author = user,
-						       source = 'http://127.0.0.1:8000/',
-						       origin = 'huh',
-						       description = contents[i][0:97] + '...',
-						       visibility = 'FRIENDS',
-						       visibleTo = '',
-				                       )
-			    myImg = Img.objects.create(associated_post = post,
-							 myImg = lilavatar )
-			    post.origin = 'http://' + request.get_host() + '/api' + reverse('post_detail', kwargs={'post_id': str(post.id) })
-			    post.source = 'http://' + request.get_host() + '/api' + reverse('post_detail', kwargs={'post_id': str(post.id) })
-			    post.save()
-			    postlist.append(post)
-		
-			createFriendsGithubs( request ) #returns postlist of ppl youre following
-
-			#if there is nothing new to send, send an empty array
-			if(len(postlist) is 0):	
-			    return HttpResponse(status=204)	
-	#prepare the new posts to be sent to the Ajax
-			jtmp = []
-			#print(len(postlist))
-			#print(postlist[0].id)
-		
-			index = 0
-			while(index<len(postlist)):
-			   
-			    jtmp.append(model_to_dict(postlist[index]))
-			    #print(jtmp[index])
-			    jtmp[index]['image'] = ""#base64.b64encode(jtmp[index]['image']) TODO fix me
-			    jtmp[index]['associated_author'] = str(Profile.objects.get(id = jtmp[index]['associated_author']).id)
-			    jtmp[index]['id'] = str(postlist[index].id)
-			    jtmp[index]['published'] = json.dumps(dateutil.parser.parse(pubtime[index] ).strftime('%B %d, %Y, %I:%M %p'))
-			    jtmp[index]['published'] = jtmp[index]['published'][1:-1]
-			    jtmp[index]['displayName'] = user.displayName #will have to make two of these to compare if its the current user's or not
-			    jtmp[index]['currentId'] = str(user.id) #current logged in user's id #
-			    index += 1
-
-			#print(json.dumps(jtmp))
-			return HttpResponse(json.dumps(jtmp),content_type = "application/json")
-
 #def createFriendsGithubs(request):
 @login_required(login_url = '/login/')
 def createGithubPosts(request):
@@ -411,7 +306,9 @@ def createGithubPosts(request):
         resp = requests.get("https://api.github.com/users/" + fgithubs[index] + "/events") #gets newest to oldest events
 	
 	jdata.append(resp.json())
-	if('https://developer.github.com/v3/#rate-limiting' in jdata[index]): #limit has been exceeded, wait 1 hour
+	
+	if('https://developer.github.com/v3/#rate-limiting' in jdata[index][0]): #limit has been exceeded, wait 1 hour
+	    print "Wait an hour -- Github request limit exceeded"
 	    return HttpResponse(status=204)
 
 	index += 1
@@ -457,8 +354,8 @@ def createGithubPosts(request):
 		              content= lilavatar + "<p>" + contents[-1] ,
 			      published=pubtime[-1],
 			      associated_author = all_profiles[index2],
-			      source = 'http://127.0.0.1:8000/',
-			      origin = 'huh',
+			      source = request.META.get('HTTP_REFERER'),#should pointto author/postid
+			      origin = request.META.get('HTTP_REFERER'),
 			      description = contents[-1][0:97] + '...',
 			      visibility = 'FRIENDS',
 			      visibleTo = '',
@@ -467,7 +364,7 @@ def createGithubPosts(request):
 		 				       myImg = lilavatar )
 		post.origin = 'http://' + request.get_host() + '/api' + reverse('post_detail', kwargs={'post_id': str(post.id) })
 		post.source = 'http://' + request.get_host() + '/api' + reverse('post_detail', kwargs={'post_id': str(post.id) })
-		#post.save()
+		post.save()
 		postlist.append(post)
 		print len(postlist)
 	    index2 +=1
@@ -491,7 +388,7 @@ def createGithubPosts(request):
        jtmp[index]['id'] = str(postlist[index].id)
        jtmp[index]['published'] = json.dumps(dateutil.parser.parse(pubtime[index] ).strftime('%B %d, %Y, %I:%M %p'))
        jtmp[index]['published'] = jtmp[index]['published'][1:-1]
-       jtmp[index]['displayName'] = user.displayName
+       jtmp[index]['displayName'] = str(Profile.objects.get(id = jtmp[index]['associated_author']).displayName)
        jtmp[index]['currentId'] = str(user.id) #current logged in user's id #
        index += 1
 
